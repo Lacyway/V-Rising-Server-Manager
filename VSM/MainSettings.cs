5ebe1d1392128fe758eb252ff95b20ef65086bcb
Tim Westring <tim_westring@hotmail.com>
2023-06-11 09:53:45 +0200

    Updated version

--- VSM/MainSettings.cs
+++ VSM/MainSettings.cs
@@ -247,7 +247,7 @@ namespace VRisingServerManager
             get => _lastUpdateTime;
             set => SetField(ref _lastUpdateTime, value);
         }
-        private string _version = "3.2b";
+        private string _version = "3.2.1b";
         public string Version
         {
             get => _version;
